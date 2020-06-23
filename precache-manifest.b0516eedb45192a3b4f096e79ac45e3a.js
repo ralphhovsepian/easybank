bc35fc950f0875153c855f3f27d9a6e598373711
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-06-23 15:05:38 +0400

    Updates

---
+++ precache-manifest.b0516eedb45192a3b4f096e79ac45e3a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23285eb1aaae1e38ef8f47cda88772a",
+    "url": "/easybank/index.html"
+  },
+  {
+    "revision": "3de4a4d9aa6ce6d547d7",
+    "url": "/easybank/static/css/main.12c3664c.chunk.css"
+  },
+  {
+    "revision": "499d69d40a0bf8922c16",
+    "url": "/easybank/static/js/2.67728ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/easybank/static/js/2.67728ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de4a4d9aa6ce6d547d7",
+    "url": "/easybank/static/js/main.040cb0fd.chunk.js"
+  },
+  {
+    "revision": "b17b429704d25d4675e3",
+    "url": "/easybank/static/js/runtime-main.e87b0b0f.js"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/easybank/static/media/bgintrodesktop.9cea2f98.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/easybank/static/media/confetti.1510e4a2.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/easybank/static/media/currency.0f583e2b.jpg"
+  },
+  {
+    "revision": "9b15027b930fe38c1682698dd366b7d6",
+    "url": "/easybank/static/media/footerlogo.9b15027b.svg"
+  },
+  {
+    "revision": "db3d89069f8292defec0f180493a8817",
+    "url": "/easybank/static/media/icon-api.db3d8906.svg"
+  },
+  {
+    "revision": "d4cdc9d6b3c930ddaff8dc79acc005a1",
+    "url": "/easybank/static/media/icon-budgeting.d4cdc9d6.svg"
+  },
+  {
+    "revision": "2fc9c776f2bb1b98e1d23bc813261bd3",
+    "url": "/easybank/static/media/icon-onboarding.2fc9c776.svg"
+  },
+  {
+    "revision": "7a9111dbfa3b4b2ea0d088435bfe1d76",
+    "url": "/easybank/static/media/icon-online.7a9111db.svg"
+  },
+  {
+    "revision": "fe5c6dcee699e86a25a18112801ec30f",
+    "url": "/easybank/static/media/logo.fe5c6dce.svg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/easybank/static/media/phones.edc63e85.png"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/easybank/static/media/plane.c619f6cb.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/easybank/static/media/resto.b561f8d5.jpg"
+  }
+]);
